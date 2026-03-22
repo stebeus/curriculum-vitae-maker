6eaca2205d85d96bb563c178862751bfa2828155
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-22 18:57:05 -0300

    feat: add ResumeSection component

---
+++ src/components/ui/ResumeSection.jsx
@@ -0,0 +1,16 @@
+function ResumeSection({ title, subtitle, dates, children }) {
+  const [startDate, endDate] = dates;
+
+  return (
+    <section id={title}>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      {children}
+      <p class="dates">
+        {startDate} – {endDate}
+      </p>
+    </section>
+  );
+}
+
+export { ResumeSection };
