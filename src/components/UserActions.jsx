4125b9babba0e02a7a1ce670f028e4f7cbd8b8f2
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-22 12:05:31 -0300

    feat: add UserActions component

---
+++ src/components/UserActions.jsx
@@ -0,0 +1,22 @@
+import { Button, createManyButtons } from './ui/Button.jsx';
+
+function UserActions() {
+  const buttons = createManyButtons(
+    ['action', 'clear', 'clearForm', 'Clear resume'],
+    ['download', 'curriculum.html', 'downloadFile', 'Download'],
+  );
+
+  const createButton = ({ dataAttrName, dataAttrValue, handler, children }) => (
+    <Button
+      dataAttrName={dataAttrName}
+      dataAttrValue={dataAttrValue}
+      onClick={handler}
+    >
+      {children}
+    </Button>
+  );
+
+  return <div className="actions">{buttons.map(createButton)}</div>;
+}
+
+export { UserActions };
