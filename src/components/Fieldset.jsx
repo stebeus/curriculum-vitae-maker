c0576c3cedf9ff621fffc80880b9a281c2f245da
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-23 13:33:52 -0300

    feat: add save button to Fieldset

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -1,8 +1,11 @@
+import { Button } from './ui/Button.jsx';
+
 function Fieldset({ legend, children }) {
   return (
     <fieldset>
       <legend>{legend}</legend>
       {children}
+      <Button label="Save" />
     </fieldset>
   );
 }
