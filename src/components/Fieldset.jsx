d9257fb4481659580128c9bcb17c017be49358bb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-23 16:33:15 -0300

    feat: add handler for setting if fields are read-only

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import { Button } from './ui/Button.jsx';
 import { createFieldData, Field } from './ui/Field.jsx';
 
 function Fieldset({ legend, fields }) {
+  const [IsReadonly, setIsReadonly] = useState(false);
+
+  const handleSetIsReadonly = () => {
+    setIsReadonly(!IsReadonly);
+  };
+
   const createField = ({ key, name, type }) => (
-    <Field key={key} name={name} type={type} />
+    <Field key={key} name={name} type={type} hasReadOnlyProp={IsReadonly} />
   );
 
   return (
     <fieldset>
       <legend>{legend}</legend>
       {fields.map(createField)}
-      <Button label="Save" />
+      <Button label="Save" handler={handleSetIsReadonly} />
     </fieldset>
   );
 }
