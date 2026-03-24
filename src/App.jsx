0a6f853423c5379bcc6c912b0c2273717712937a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-24 09:31:54 -0300

    style: use inline export for App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Footer } from './components/Footer.jsx';
 import { Header } from './components/Header.jsx';
 import { ResumeForm } from './components/ResumeForm.jsx';
 
-function App() {
+export function App() {
   return (
     <>
       <Header />
@@ -13,5 +13,3 @@ function App() {
     </>
   );
 }
-
-export { App };
