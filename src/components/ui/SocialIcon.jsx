2f5c3d0b90b81404d24e1fb1fb5098a2a89ae868
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-24 14:46:21 -0300

    feat!: add alt prop for accessible icon labels

--- src/components/ui/SocialIcon.jsx
+++ src/components/ui/SocialIcon.jsx
@@ -1,8 +1,8 @@
 import { BASE_URL } from '../../constants.js';
 
-export function SocialIcon({ brand }) {
+export function SocialIcon({ brand, alt }) {
   return (
-    <svg className="social-icon" role="presentation" aria-hidden="true">
+    <svg className="social-icon" role="img" aria-label={alt}>
       <use href={`${BASE_URL}social-icons.svg#${brand}`}></use>
     </svg>
   );
