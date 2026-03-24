faa82311eb18774bb6cc1f437418ea8289dff66f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-24 09:24:25 -0300

    refactor!: improve Fieldset component strructure
    Use explicit and meaningful names for the field factory parameters and
    component states.

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -2,33 +2,31 @@ import { useState } from 'react';
 import { Button } from './ui/Button.jsx';
 import { createFieldData, Field } from './ui/Field.jsx';
 
-function Fieldset({ legend, fields }) {
-  const [IsReadonly, setIsReadonly] = useState(false);
-  const [btnLabel, setBtnLabel] = useState('Save');
+export function Fieldset({ legend, fields }) {
+  const [isFieldSaved, setIsFieldSaved] = useState(false);
+  const [buttonLabel, setButtonLabel] = useState('Save');
 
-  const handleSetIsReadonly = () => {
-    setIsReadonly(!IsReadonly);
-    setBtnLabel(btnLabel === 'Save' ? 'Edit' : 'Save');
+  const handleFieldSave = () => {
+    setIsFieldSaved(!isFieldSaved);
+    setButtonLabel(buttonLabel === 'Save' ? 'Edit' : 'Save');
   };
 
-  const createField = ({ key, name, type }) => (
-    <Field key={key} name={name} type={type} hasReadOnlyProp={IsReadonly} />
+  const createField = ({ key, label, type }) => (
+    <Field key={key} label={label} type={type} isReadOnly={isFieldSaved} />
   );
 
   return (
     <fieldset>
       <legend>{legend}</legend>
       {fields.map(createField)}
-      <Button label={btnLabel} handler={handleSetIsReadonly} />
+      <Button label={buttonLabel} handler={handleFieldSave} />
     </fieldset>
   );
 }
 
-const createFieldsetData = (...fieldsets) =>
+export const createFieldsetData = (...fieldsets) =>
   fieldsets.map(([legend, ...fields]) => ({
     key: crypto.randomUUID(),
     legend,
     fields: createFieldData(...fields),
   }));
-
-export { createFieldsetData, Fieldset };
