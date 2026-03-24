bc6896e1f7bb0a1104c1517b606c7424e81e3bbf
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-24 14:33:05 -0300

    refactor!: add icons to button

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -1,14 +1,22 @@
+import { Save, SquarePen } from 'lucide-react';
 import { useState } from 'react';
 import { Button } from './ui/Button.jsx';
 import { createFieldData, Field } from './ui/Field.jsx';
 
 export function Fieldset({ legend, fields }) {
   const [isFieldSaved, setIsFieldSaved] = useState(false);
+
   const [buttonLabel, setButtonLabel] = useState('Save');
+  const [buttonIcon, setButtonIcon] = useState(<Save />);
+
+  const switchButtonRole = (label) => {
+    setButtonLabel(label === 'Save' ? 'Edit' : 'Save');
+    setButtonIcon(label === 'Save' ? <SquarePen /> : <Save />);
+  };
 
   const handleFieldSave = () => {
     setIsFieldSaved(!isFieldSaved);
-    setButtonLabel(buttonLabel === 'Save' ? 'Edit' : 'Save');
+    switchButtonRole(buttonLabel);
   };
 
   const createField = ({ key, label, type }) => (
@@ -19,7 +27,10 @@ export function Fieldset({ legend, fields }) {
     <fieldset>
       <legend>{legend}</legend>
       {fields.map(createField)}
-      <Button label={buttonLabel} handler={handleFieldSave} />
+      <Button handler={handleFieldSave}>
+        {buttonLabel}
+        {buttonIcon}
+      </Button>
     </fieldset>
   );
 }
