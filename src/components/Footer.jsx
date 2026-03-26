e13954c5c7a7e2cf21debc18cf6bade4218cab24
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-26 07:05:24 -0300

    refactor!: improve footer structure to facilitate styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { SocialIcon } from './ui/SocialIcon.jsx';
 export function Footer() {
   return (
     <footer>
-      <p>
+      <p className="copyright">
         {`© Stebeus 2026. `}
         <Link to="http://opensource.org/license/mit">MIT License</Link>
-        <Link to="http://github.com/stebeus">
-          <SocialIcon brand="github" alt="GitHub profile" />
-        </Link>
       </p>
+      <Link to="http://github.com/stebeus">
+        <SocialIcon brand="github" alt="GitHub profile" />
+      </Link>
     </footer>
   );
 }
